7da75fac4d31055c27ebacd8df451c15033ce153
Philipp <Philipp@ELITEBOOK>
2017-04-06 15:01:51 +0200

    Add project files.

---
+++ src/tempusOIDCServer/Config.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace tempusOIDCServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApitResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = {"api1"}
+                }
+            };
+        }
+    }
+}
+    
